7e0d5288b8b9053469a4357d1495a578ac4ad716
dseelig <seeliger.biosoft@gmail.com>
2015-08-22 13:59:02 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os,sys
+from distutils.core import setup
+
+setup (name = 'cmdline',
+       version = '1.0.0',
+       description = 'Module for commandline parsing',
+       author = 'Daniel Seeliger',
+       author_email = 'seeliger.biosoft@gmail.de',
+       url = 'http://code.google.com/p/pmx/',
+       packages = ['cmdline']
+       )
+
